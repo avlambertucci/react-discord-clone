104881ff11edc08fd6c7e075317265b8c6c8261a
avlambertucci <armandolambertucci@gmail.com>
2020-11-16 12:42:05 -0300

    ServerName added

--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import {Grid} from './styles'
 import ServerList from '../ServerList'
+import ServerName from '../ServerName'
+
+
 
 
 const Layout: React.FC = ()=>{
     return(
         <Grid>
             <ServerList />
+            <ServerName />
         </Grid>
     )
 }
